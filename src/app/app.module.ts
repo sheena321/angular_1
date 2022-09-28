ac36576a51040ae3dfc4f629180d9533809da79b
sheena <sheena3891@gmail.com>
2022-09-28 15:07:34 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FirstComponent } from './first/first.component';
+import { SecComponent } from './sec/sec.component';
+import { ThirdComponent } from './third/third.component';
+import { ShowComponent } from './show/show.component';
+import { PlacementComponent } from './placement/placement.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FirstComponent,
+    SecComponent,
+    ThirdComponent,
+    ShowComponent,
+    PlacementComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
